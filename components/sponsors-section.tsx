ec7d38ee283cf069479c5867fee2b059f57cd7ca
kmuminiolympic-blip <kmuminiolympic@gmail.com>
2026-05-22 21:34:20 +0900

    Update sponsors-section.tsx

--- components/sponsors-section.tsx
+++ components/sponsors-section.tsx
@@ -9,7 +9,7 @@ const sponsors = [
   { id: 3, name: "TMT 피자", logo: "/images/sponsors/sponsor-c.png", website: "https://naver.me/xE6RKOw6" },
   { id: 4, name: "사조 팝콘", logo: "/images/sponsors/sponsor-d.png", website: "https://naver.me/xE6RKOw6" },
   { id: 5, name: "정육회", logo: "/images/sponsors/정육회.jpg", website: "https://www.instagram.com/yukhoe_jung_kmu/" },
-  { id: 6, name: "수기소금", logo: "/images/sponsors/수기수금.png", website: "https://www.instagram.com/sugisogeum.kmu/" },
+  { id: 6, name: "수기소금", logo: "/images/sponsors/수기소금.png", website: "https://www.instagram.com/sugisogeum.kmu/" },
   { id: 7, name: "EARTH, US", logo: "/images/sponsors/EARTH,US.jpg", website: "https://www.instagram.com/earthuskr/" },
   { id: 8, name: "눈을 담다", logo: "/images/sponsors/sponsor-h.png", website: "https://naver.me/GiaQpMeh" },
   { id: 9, name: "풍동", logo: "/images/sponsors/sponsor-i.png", website: "https://www.instagram.com/poongdong_kmu/" },
